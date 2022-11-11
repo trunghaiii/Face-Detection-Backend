d505fc8ca8642d53c06c6844b0b6db20b909e4ce
Hai <trunghai2422001@gmail.com>
2022-11-10 21:10:21 -0500

    fix3

--- controllers/register.js
+++ controllers/register.js
@@ -33,7 +33,7 @@ const handleRegister = (req, res, db, bcrypt) => {
             .catch(trx.rollback)
     })
 
-        .catch(err => res.status(400).json(err))
+        .catch(err => res.status(400).json('can not register'))
 
 
 }
