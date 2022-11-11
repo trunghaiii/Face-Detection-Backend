d7e78301f028ecedcec8fd88367e8948cb174ac7
Hai <trunghai2422001@gmail.com>
2022-11-10 20:43:11 -0500

    fix2

--- server.js
+++ server.js
@@ -13,10 +13,7 @@ const db = knex({
     client: 'pg',
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: true
-        // user: 'postgres',
-        // password: "haiman123@",
-        // database: 'face-detection'
+        ssl: true,
     }
 });
 
